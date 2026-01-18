34e78bf9b06d6e2eae9db7dd92a36df04abeaea0
Adam <adamff.me@gmail.com>
2026-01-18 15:22:54 +0100

    build.gradle.kts: bump versions to 2.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 29
         //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 26
-        versionName = "2.5.4"
+        versionCode = 27
+        versionName = "2.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
